55c4816db3ca9476b8498da9db50f641600e46c9
C-alcium <62979683+C-alcium@users.noreply.github.com>
2021-01-22 20:03:00 +0000

    Catch up to old position with new typescript project

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="header navbar">
+        <div class="navbar-start"> 
+            <router-link class="navbar-item" v-for="route in routes" :key="route.path" :to="route.path">
+                {{ route.name }}
+            </router-link>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+declare interface Route {
+    name: string;
+    path: string;
+}
+
+export default defineComponent({
+    name:'Header',
+    data() {
+        return {
+            routes: new Array<Route>()
+        }
+    },
+    created() {
+        this.$router.options.routes.forEach(route => {
+            this.routes.push({
+                name: route.name as string,
+                path: route.path as string
+            })
+        })
+    }
+})
+</script>
+
+<style>
+    
+</style>
