8edd51ea98b4c9d0aff5ceb9e42d3c30b2bd77f2
C-alcium <62979683+C-alcium@users.noreply.github.com>
2021-01-23 12:23:13 +0000

    Build markdown renderer

---
+++ src/assets/content/en/home.md
@@ -0,0 +1,6 @@
+# What is the purpose of this website?
+
+The purpose of finance-quickstart is to provide an overview of financial terms for people looking to start FIRE.
+Similarly, you can find FIRE differences in supported languages throughout the rest of this site.
+At the moment, the current goal is to cover the base topics in English, and later add other languages.
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <p> 
-    Home Content
-  </p>
+  <div class="content" v-html="content"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import MarkdownIt from 'markdown-it';
+import HomeContent from '@/assets/content/en/home.md';
 
 export default defineComponent({
   name: 'Home',
+  data() {
+    return {
+      content: new MarkdownIt().render(HomeContent)
+    }
+  },
 });
 </script>
